4b54f4bd9c7c16789b32d24cd7d28dcd6ca7f545
Rex <mortensenrex@gmail.com>
2025-01-25 01:50:43 +0100

    meget mere tilføjet, men virker ikke

--- sketch.js
+++ sketch.js
@@ -11,22 +11,28 @@ function setup() {
 function draw() {
   background(100);
 
+  if(nodes.length > 1){
+   
+    let t = slider.value()
+    let allLines = []
+    let interpoint = calcRecPoints(nodes, t)
 
+    for(let i = 0; i < interpoint.length - 1; i++){
 
-  for(i = 0; i < nodes.length - 1; i++){
-
-    let p1 = nodes[i]
-    let p2 = nodes[i+1]
+    let p1 = interpoint[i]
+    let p2 = interpoint[i+1]
 
     stroke(0)
     line(p1.x, p1.y, p2.x, p2.y)
-
-    let t = slider.value()
-    
-    let difX = lerp(p1.x, p2.x, t)
-    let difY = lerp(p1.y, p2.y, t)
+  
     fill(255, 0, 0)
-    circle(difX, difY, 10)
+    circle(p1.x, p1.y, 10)
+
+    }
+    
+    let lastPoint = interpoint[interpoint.length - 1]
+    fill(0, 255, 0)
+    circle(lastPoint.x, lastPoint.y, 10)
 
   }
 
@@ -36,6 +42,13 @@ function draw() {
     fill(255)
     circle(node.x, node.y, 20)
 
+    for (let i = 0; i < allLines.length; i++) {
+      let linePart = allLines[i]
+
+      line(linePart.p1.x, linePart.p1.y, linePart.p2.x, linePart.p2.y)
+
+
+    }
   }
   
   if(pointSelected != null){
@@ -46,7 +59,33 @@ function draw() {
 
 }
 
+function calcRecPoints(points, t, allLines){
+  let newPoints = []
+
+  for(i = 0; i < points.length - 1; i++){
+
+    let p1 = points[i]
+    let p2 = points[i+1]
+    
+    let difX = lerp(p1.x, p2.x, t)
+    let difY = lerp(p1.y, p2.y, t)
+    newPoints.push({ x: difX, y: difY})
+
+    allLines.push({ p1, p2 })
+  }
+
+  if(newPoints.length > 1){
+    return calcRecPoints(newPoints, t, allLines)
+  } else {
+    return newPoints
+  }
+}
+
 function mousePressed(){
+
+  if (mouseY < 40 && mouseX < 150) {
+    return true
+  }
 
   for(let i = 0; i< nodes.length; i++){
 
