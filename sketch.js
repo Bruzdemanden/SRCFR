4d77e02e214e7094f5ecaf2013c54ea846f7d169
Rex <mortensenrex@gmail.com>
2025-02-17 14:41:10 +0100

    Tilføjede flere muligheden for at lave flere individuelle kurver istedet for kun 1

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let slider
 let resetButton
 let animating = false
 let animatingCheckbox
-let nodes = []
-let path = []
+let nodes = [[]]
+let path = [[]]
 let t = 0
 let pointSelected = null
 
@@ -30,56 +30,71 @@ function draw() {
   textSize(16)
   text('t = ' + t.toFixed(2), 18, 50)
 
-  //Auto update of t
-  if(animating){
-    t += 0.01
-    if(t > 1){
-      t = 0
-      path = []
-    }
-    slider.value(t)
-  } else {
-    t = slider.value()
-  }
-
-  noFill()
-  stroke(t*100, t*255, 0)
-  strokeWeight(1)
-
-  beginShape()
-  for(let i = 0; i < path.length; i++){
-    let point = path[i]
-    vertex(point.x, point.y)
-  }
-  endShape()
-
-  if(nodes.length > 1){
-    let interpoint = calcRecPoints(nodes, t)
-    
-    let lastPoint = interpoint[interpoint.length - 1]
-    fill(0, 255, 0)
-    circle(lastPoint.x, lastPoint.y, 10)
-
-    path.push(lastPoint)
-  }
-
-  // Draws the white nodes
-  for(let i = 0; i < nodes.length; i++){
-
-    let node = nodes[i]
-    stroke(0)
-    strokeWeight(1)
-    
-    fill(255)
-    circle(node.x, node.y, 20)
-
-  }
   
+  for(let i2=0; nodes.length > i2; i2++){
+    //Auto update of t
+    if(animating){
+      t += i2 == 0 ? 0.01 : 0
+      if(t > 1){
+        t = 0
+        path = [[]]
+        for (let o = 0; o < nodes.length; o++) {
+          path.push([])
+        }
+      }
+      slider.value(t)
+    } else {
+      t = slider.value()
+    }
+  
+    noFill()
+    stroke(t*100, t*255, 0)
+    strokeWeight(1)
+  
+    beginShape()
+    for(let i = 0; i < path[i2].length; i++){
+      let point = path[i2][i]
+      vertex(point.x, point.y)
+    }
+    endShape()
+
+    if(nodes[i2].length > 1){
+      let tangentVector = ({x: 0, y: 0})
+  
+  
+      tangentVector.x = (calcRecPoints(nodes[i2], t+0.001)[0].x - calcRecPoints(nodes[i2], t-0.001)[0].x)/(0.001-(-0.001))
+      tangentVector.y = (calcRecPoints(nodes[i2], t+0.001)[0].y - calcRecPoints(nodes[i2], t-0.001)[0].y)/(0.001-(-0.001))
+  
+  
+      
+      let interpoint = calcRecPoints(nodes[i2], t)
+     // fill(255, 0, 0)
+     // line(interpoint[0].x, interpoint[0].y, interpoint[0].x+tangentVector.x, interpoint[0].y+tangentVector.y)
+      
+      let lastPoint = interpoint[interpoint.length - 1]
+      fill(0, 255, 0)
+      circle(lastPoint.x, lastPoint.y, 10)
+  
+      path[i2].push(lastPoint)
+    }
+  
+    // Draws the white nodes[i2]
+    for(let i = 0; i < nodes[i2].length; i++){
+  
+      let node = nodes[i2][i]
+      stroke(0)
+      strokeWeight(1)
+      fill(255)
+      circle(node.x, node.y, 20)
+    }
+  }
   if(pointSelected != null){
 
-    nodes[pointSelected].x = mouseX
-    nodes[pointSelected].y = mouseY
+    nodes[pointSelected.liste][pointSelected.index].x = mouseX
+    nodes[pointSelected.liste][pointSelected.index].y = mouseY
   }
+
+  
 }
 
 function calcRecPoints(points, t){
@@ -119,15 +134,17 @@ function mousePressed(){
     return true
   }
 
-  for(let i = 0; i< nodes.length; i++){
-    let d = dist(mouseX, mouseY, nodes[i].x, nodes[i].y)
-
-    if(d < 20){
-      pointSelected = i
-      return
-    } 
+  for (let i2 = 0; i2 < nodes.length; i2++) {
+    for(let i = 0; i< nodes[i2].length; i++){
+      let d = dist(mouseX, mouseY, nodes[i2][i].x, nodes[i2][i].y)
+  
+      if(d < 20){
+        pointSelected = {index: i, liste: i2}
+        return
+      } 
+    }
   }
-  nodes.push({x: mouseX, y: mouseY})
+  nodes[nodes.length-1].push({x: mouseX, y: mouseY})
 }
 
 //Should make sense
@@ -137,7 +154,14 @@ function mouseReleased(){
 
 //Same here ig
 function resetCurves(){
-  nodes = []
+  nodes = [[]]
+  path = [[]]
   slider.value(0)
-  path = []
+}
+
+function keyPressed() {
+  if (keyCode == 78) {
+    nodes.push([{x: nodes[nodes.length-1][nodes[nodes.length-1].length-1].x, y: nodes[nodes.length-1][nodes[nodes.length-1].length-1].y}])
+    path.push([])
+  }
 }
